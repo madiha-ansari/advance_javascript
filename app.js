3e88cb77e78dcc985f94e089072e9dd3c1059b24
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-10-04 11:19:11 +0500

    advance js

---
+++ app.js
@@ -0,0 +1,16 @@
+// template literals
+// let templateLiteral = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, quia quisquam. Culpa corporis officiis eius eum dolorem quibusdam veritatis ducimus ipsa accusamus aut! Obcaecati molestiae numquam, labore molestias natus nisi.`;
+// console.log(templateLiteral);
+
+// destructing
+// var arrary = ["red","pink"];
+// var [,arrary2] = arrary;
+// console.log(arrary2);
+
+
+// nested
+// var numbers = [["one","two"],"three"];
+// var val = [[,num1],num3]= numbers;
+// console.log(num1);
+
+
